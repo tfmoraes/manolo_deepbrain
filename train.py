0d3655b59c5aa10c73b81b7fa5a6a44f20307f05
Thiago Franco de Moraes <totonixsame@gmail.com>
2020-02-02 16:55:12 -0300

    generating patches

--- train.py
+++ train.py
@@ -21,10 +21,103 @@ from scipy.ndimage import rotate
 
 def apply_transform(image, rot1, rot2):
     if rot1 > 0:
-        image = rotate(image, angle=rot1, axes=(1, 0), output=np.float32, order=0, prefilter=False)
+        image = rotate(
+            image, angle=rot1, axes=(1, 0), output=np.float32, order=0, prefilter=False
+        )
     if rot2 > 0:
-        image = rotate(image, angle=rot2, axes=(2, 1), output=np.float32, order=0, prefilter=False)
+        image = rotate(
+            image, angle=rot2, axes=(2, 1), output=np.float32, order=0, prefilter=False
+        )
     return image
+
+
+def get_epoch_size(files, patch_size=SIZE):
+    size = 0
+    for image_filename, mask_filename in files:
+        sz, sy, sx = nb.load(str(image_filename)).get_shape()
+        size += int(
+            np.ceil(sz / patch_size)
+            * np.ceil(sy / patch_size)
+            * np.ceil(sx / patch_size)
+        )
+    return size
+
+
+def gen_patches(image, mask, patch_size=SIZE):
+    sz, sy, sx = image.shape
+    i_cuts = itertools.product(
+        range(0, sz, patch_size), range(0, sy, patch_size), range(0, sx, patch_size)
+    )
+
+    print(image.shape, len(list(i_cuts)))
+
+    for iz, iy, ix in i_cuts:
+        sub_image = np.zeros(
+            shape=(patch_size, patch_size, patch_size), dtype="float32"
+        )
+        sub_mask = np.zeros_like(sub_image)
+
+        _sub_image = image[
+            iz : iz + patch_size, iy : iy + patch_size, ix : ix + patch_size
+        ]
+        _sub_mask = mask[
+            iz : iz + patch_size, iy : iy + patch_size, ix : ix + patch_size
+        ]
+
+        sz, sy, sx = _sub_image.shape
+
+        sub_image[0:sz, 0:sy, 0:sx] = _sub_image
+        sub_mask[0:sz, 0:sy, 0:sx] = _sub_mask
+
+        yield sub_image, sub_mask
+
+
+def load_models_patches(files, patch_size=SIZE, batch_size=BATCH_SIZE):
+    transformations = list(itertools.product(range(0, 360, 90), range(0, 360, 90)))
+
+    for image_filename, mask_filename in files:
+        image = nb.load(str(image_filename)).get_fdata()
+        mask = nb.load(str(mask_filename)).get_fdata()
+        image = model.image_normalize(image)
+        mask = model.image_normalize(mask)
+        for rot1, rot2 in transformations:
+            t_image = apply_transform(image, rot1, rot2)
+            t_mask = apply_transform(mask, rot1, rot2)
+
+            print(image_filename, rot1, rot2)
+
+            for sub_image, sub_mask in gen_patches(t_image, t_mask, patch_size):
+                yield (sub_image, sub_mask)
+
+
+def gen_train_arrays(files, patch_size=SIZE, batch_size=BATCH_SIZE):
+    transformations = list(itertools.product(range(0, 360, 90), range(0, 360, 90)))
+    size = get_epoch_size(files, patch_size) * len(transformations)
+    yield int(np.ceil(size / batch_size))
+    while True:
+        images = []
+        masks = []
+        for image, mask in load_models_patches(files, patch_size, batch_size):
+            images.append(image)
+            masks.append(mask)
+
+            if len(images) == batch_size:
+                _images = np.array(images).reshape(
+                    -1, patch_size, patch_size, patch_size, 1
+                )
+                _masks = np.array(masks).reshape(
+                    -1, patch_size, patch_size, patch_size, 1
+                )
+                yield _images, _masks
+                images = []
+                masks = []
+
+        if images:
+            _images = np.array(images).reshape(
+                -1, patch_size, patch_size, patch_size, 1
+            )
+            _masks = np.array(masks).reshape(-1, patch_size, patch_size, patch_size, 1)
+            yield _images, _masks
 
 
 def load_models(files, batch_size=1):
@@ -40,7 +133,9 @@ def load_models(files, batch_size=1):
         for image_filename, mask_filename in files:
             image = nb.load(str(image_filename)).get_fdata()
             mask = nb.load(str(mask_filename)).get_fdata()
-            image = resize(image, (SIZE, SIZE, SIZE), mode="constant", anti_aliasing=True)
+            image = resize(
+                image, (SIZE, SIZE, SIZE), mode="constant", anti_aliasing=True
+            )
             mask = resize(mask, (SIZE, SIZE, SIZE), mode="constant", anti_aliasing=True)
             image = model.image_normalize(image)
             mask = model.image_normalize(mask)
@@ -61,14 +156,14 @@ def load_models(files, batch_size=1):
 
 def train(kmodel, deepbrain_folder):
     cc359_files = file_utils.get_cc359_filenames(deepbrain_folder)
-    nfbs_files = file_utils.get_nfbs_filenames(deepbrain_folder)
-    files = cc359_files + nfbs_files
+    #  nfbs_files = file_utils.get_nfbs_filenames(deepbrain_folder)
+    files = cc359_files  # + nfbs_files
     random.shuffle(files)
 
     training_files = files[: int(len(files) * 0.75)]
     testing_files = files[int(len(files) * 0.75) :]
-    training_files_gen = load_models(training_files, BATCH_SIZE)
-    testing_files_gen = load_models(testing_files, BATCH_SIZE)
+    training_files_gen = gen_train_arrays(training_files, SIZE, BATCH_SIZE)
+    testing_files_gen = gen_train_arrays(testing_files, SIZE, BATCH_SIZE)
     len_training_files = next(training_files_gen)
     len_testing_files = next(testing_files_gen)
 
