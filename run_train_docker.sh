d430bb8b3f0e897bd85010a96a0c28f2bb61f8de
Thiago Franco de Moraes <totonixsame@gmail.com>
2020-06-27 21:18:41 -0300

    added script to start horovod and tensorboard

--- run_train_docker.sh
+++ run_train_docker.sh
@@ -4,4 +4,4 @@
 docker build -t manolo_deepbrain .
 
 # Runing the docker container
-docker run --gpus all -v `pwd`:/code --rm manolo_deepbrain /bin/bash run_horovod_tensorboard.sh
+docker run --runtime=nvidia -it --rm -v `pwd`:/code -p 6006:6006 --gpus all --name paulo_nodulos_pulmao manolo_deepbrain_horovod /bin/bash run_horovod_tensorboard.sh
